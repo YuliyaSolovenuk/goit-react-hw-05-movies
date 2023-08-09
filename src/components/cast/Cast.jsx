93a5732daa9e04abffd54dbe9dcb0ad61423ce2d
Yulia Solovenuk <121509951+YuliyaSolovenuk@users.noreply.github.com>
2023-08-09 11:00:55 +0300

    cast

--- src/components/cast/Cast.jsx
+++ src/components/cast/Cast.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getMovieCast } from 'services/TmdbAPI';
+import fallbackImage from '../../image/no-photo.png';
+import css from './Cast.module.css';
 
 const Cast = () => {
   const [cast, setCast] = useState(null);
@@ -20,16 +22,22 @@ const Cast = () => {
     fetchMovieCast();
   }, [movieId]);
 
+  if (!cast) return;
+
   return (
     <div>
       <ul>
         {cast ? (
           cast.map(({ id, name, character, profile_path }) => {
             return (
-              <li key={id}>
+              <li className={css.itemActor} key={id}>
                 <img
+                  className={css.img}
                   src={`https://image.tmdb.org/t/p/w200/${profile_path}`}
                   alt={name}
+                  onError={e => {
+                    e.target.src = fallbackImage;
+                  }}
                 />
                 <h4>{name}</h4>
                 <p>Character:&nbsp;{character}</p>
